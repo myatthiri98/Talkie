c3cea8bb558875f8f33b255bbe6b7e2a30bc4e56
Myatthiri <myatthirikhin1998@gmail.com>
2025-07-20 23:13:31 +0630

    feat: add custom Button component and enhance authentication screen layout with Google sign-in option

--- app/(auth)/index.tsx
+++ app/(auth)/index.tsx
@@ -1,3 +1,4 @@
+import { Button } from "@/components/Button";
 import CustomText from "@/components/Text";
 import { Image, SafeAreaView, View } from "react-native";
 
@@ -8,19 +9,48 @@ export default function Index() {
         flex: 1,
       }}
     >
-      <View style={{ gap: 20, alignItems: "center" }}>
-        <Image
-          source={require("@/assets/images/logo.png")}
+      <View
+        style={{
+          flex: 1,
+          justifyContent: "center",
+          alignItems: "center",
+          padding: 16,
+        }}
+      >
+        <View style={{ flex: 0.1 }} />
+        <View style={{ gap: 20, alignItems: "center" }}>
+          <Image
+            source={require("@/assets/images/logo.png")}
+            style={{
+              width: 100,
+              height: 100,
+              resizeMode: "contain",
+            }}
+          />
+          <CustomText style={{ fontSize: 32, fontWeight: "bold" }}>
+            Modern Chat App
+          </CustomText>
+          <CustomText>The best chat app for your business</CustomText>
+        </View>
+        <View style={{ flex: 1 }} />
+        <Button style={{ marginBottom: 20 }}>Sign In With passkey</Button>
+        <Button
           style={{
-            width: 100,
-            height: 100,
-            resizeMode: "contain",
+            flexDirection: "row",
+            gap: 10,
+            alignItems: "center",
+            justifyContent: "center",
+            marginBottom: 20,
           }}
-        />
-        <CustomText style={{ fontSize: 32, fontWeight: "bold" }}>
-          Modern Chat App
-        </CustomText>
-        <CustomText>The best chat app for your business</CustomText>
+        >
+          <Image
+            source={require("@/assets/images/google-icon.png")}
+            style={{ width: 20, height: 20 }}
+          />
+          <CustomText style={{ color: "black", fontWeight: "500" }}>
+            Sign In With Google
+          </CustomText>
+        </Button>
       </View>
     </SafeAreaView>
   );
