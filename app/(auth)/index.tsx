ca26728208d401f40e25d3371d26e9e1ac56c0e4
Myatthiri <myatthirikhin1998@gmail.com>
2025-07-21 23:15:07 +0630

    feat: enhance authentication screen with Google SSO and passkey sign-in functionality, including error handling

--- app/(auth)/index.tsx
+++ app/(auth)/index.tsx
@@ -1,8 +1,67 @@
 import { Button } from "@/components/Button";
 import CustomText from "@/components/Text";
+import { isClerkAPIResponseError, useSignIn, useSSO } from "@clerk/clerk-expo";
+import { ClerkAPIError } from "@clerk/types";
+import * as AuthSession from "expo-auth-session";
+import * as WebBrowser from "expo-web-browser";
+import { useCallback, useState } from "react";
 import { Image, SafeAreaView, View } from "react-native";
 
+WebBrowser.maybeCompleteAuthSession();
 export default function Index() {
+  const { startSSOFlow } = useSSO();
+  const { signIn, setActive } = useSignIn();
+  const [errors, setErrors] = useState<ClerkAPIError[]>([]);
+
+  const handleSignInWithGoogle = useCallback(async () => {
+    try {
+      // Start the authentication process by calling `startSSOFlow()`
+      const { createdSessionId, setActive } = await startSSOFlow({
+        strategy: "oauth_google",
+        // Defaults to current path
+        redirectUrl: AuthSession.makeRedirectUri(),
+      });
+
+      // If sign in was successful, set the active session
+      if (createdSessionId) {
+        setActive!({ session: createdSessionId });
+      } else {
+        // If there is no `createdSessionId`,
+        // there are missing requirements, such as MFA
+        // Use the `signIn` or `signUp` returned from `startSSOFlow`
+        // to handle next steps
+      }
+    } catch (err) {
+      // See https://clerk.com/docs/custom-flows/error-handling
+      // for more info on error handling
+      if (isClerkAPIResponseError(err)) setErrors(err.errors);
+      console.error(JSON.stringify(err, null, 2));
+    }
+  }, []);
+
+  const signInWithPasskey = async () => {
+    // 'discoverable' lets the user choose a passkey
+    // without auto-filling any of the options
+    try {
+      const signInAttempt = await signIn?.authenticateWithPasskey({
+        flow: "discoverable",
+      });
+
+      if (signInAttempt?.status === "complete") {
+        if (setActive !== undefined) {
+          await setActive({ session: signInAttempt.createdSessionId });
+        }
+      } else {
+        // If the status is not complete, check why. User may need to
+        // complete further steps.
+        console.error(JSON.stringify(signInAttempt, null, 2));
+      }
+    } catch (err) {
+      // See https://clerk.com/docs/custom-flows/error-handling
+      // for more info on error handling
+      console.error("Error:", JSON.stringify(err, null, 2));
+    }
+  };
   return (
     <SafeAreaView
       style={{
@@ -31,9 +90,15 @@ export default function Index() {
             Modern Chat App
           </CustomText>
           <CustomText>The best chat app for your business</CustomText>
+          <CustomText>Sign in to continue</CustomText>
+          {errors.map((error) => (
+            <CustomText key={error.code}>{error.code}</CustomText>
+          ))}
         </View>
         <View style={{ flex: 1 }} />
-        <Button style={{ marginBottom: 20 }}>Sign In With passkey</Button>
+        <Button style={{ marginBottom: 20 }} onPress={signInWithPasskey}>
+          Sign In With passkey
+        </Button>
         <Button
           style={{
             flexDirection: "row",
@@ -42,6 +107,7 @@ export default function Index() {
             justifyContent: "center",
             marginBottom: 20,
           }}
+          onPress={handleSignInWithGoogle}
         >
           <Image
             source={require("@/assets/images/google-icon.png")}
