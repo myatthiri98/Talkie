48ca6773ef623718158020534567d516c2bed397
Myatthiri <myatthirikhin1998@gmail.com>
2025-07-23 23:42:01 +0630

    chore: update Podfile.lock and Xcode project settings, add Appwrite utility for database integration

---
+++ utils/appwrite.ts
@@ -0,0 +1,25 @@
+import { Client, Databases } from "react-native-appwrite";
+
+if (!process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID) {
+  throw new Error("EXPO_PUBLIC_APPWRITE_PROJECT_ID is not set");
+}
+
+const appwriteConfig = {
+  endpoint: "https://cloud.appwrite.io/v1",
+  projectId: process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID,
+  platform: "com.myatthiri.chattalk",
+  db: process.env.EXPO_PUBLIC_APPWRITE_DATABASE_ID,
+  col: {
+    chatrooms: "687f62ce00310e1fe7a0",
+    messages: "687f62b80008545a1351",
+  },
+};
+
+const client = new Client()
+  .setEndpoint(appwriteConfig.endpoint)
+  .setProject(appwriteConfig.projectId)
+  .setPlatform(appwriteConfig.platform);
+
+const database = new Databases(client);
+
+export { appwriteConfig, client, database };
