75f3e27187e6636d05f260ee9155fb107fa1314c
myatthiri98 <myatthirikhin1998@gmail.com>
2025-07-24 16:30:19 +0630

    feat: implement room creation functionality in NewRoom component, integrating Appwrite for document storage and enhancing UI with loading state

--- app/(chat)/new-room.tsx
+++ app/(chat)/new-room.tsx
@@ -1,15 +1,44 @@
 import Input from '@/components/Input'
 import CustomText from '@/components/Text'
+import { appwriteConfig, database } from '@/utils/appwrite'
+import { Stack, useRouter } from 'expo-router'
 import { useState } from 'react'
-import { StyleSheet, View } from 'react-native'
+import { Button, StyleSheet, View } from 'react-native'
+import { ID } from 'react-native-appwrite'
 
 export default function NewRoom() {
   const [roomName, setRoomName] = useState('')
   const [roomDescription, setRoomDescription] = useState('')
   const [isLoading, setIsLoading] = useState(false)
+  const router = useRouter()
+
+  const handleCreateRoom = async () => {
+    setIsLoading(true)
+    try {
+      await database.createDocument(
+        appwriteConfig.db,
+        appwriteConfig.col.chatrooms,
+        ID.unique(),
+        {
+          title: roomName,
+          description: roomDescription,
+        },
+      )
+      router.back()
+    } catch (error) {
+      console.error(error)
+    } finally {
+      setIsLoading(false)
+    }
+  }
 
   return (
     <>
+      <Stack.Screen options={{
+        headerRight: () => (
+          <Button title={isLoading ? 'Creating...' : 'Create'} onPress={handleCreateRoom} disabled={roomName === ''} />
+        )
+      }} />
       <View style={styles.container}>
         <CustomText>New Room</CustomText>
         <Input
