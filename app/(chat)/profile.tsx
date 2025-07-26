c6b0e713d9d623daf749780ddeca05ae4f583e6a
Myatthiri <myatthirikhin1998@gmail.com>
2025-07-26 19:48:39 +0630

    feat: enhance profile component UI by adding user image, name, and email display with improved styling

--- app/(chat)/profile.tsx
+++ app/(chat)/profile.tsx
@@ -3,7 +3,7 @@ import CustomText from '@/components/Text'
 import { useAuth, useUser } from '@clerk/clerk-expo'
 import { useRouter } from 'expo-router'
 import React from 'react'
-import { View } from 'react-native'
+import { Image, StyleSheet, View } from 'react-native'
 
 const profile = () => {
   const { user } = useUser()
@@ -16,11 +16,36 @@ const profile = () => {
   }
 
   return (
-    <View>
-      <CustomText>profile</CustomText>
+    <View style={styles.container}>
+      <Image source={{ uri: user?.imageUrl }} style={styles.imageContainer} />
+      <View style={styles.infoContainer}>
+        <CustomText style={styles.userName}>{user?.fullName}</CustomText>
+        <CustomText>{user?.emailAddresses[0].emailAddress}</CustomText>
+      </View>
       <Button onPress={handleSignOut}> Sign Out</Button>
     </View>
   )
 }
 
 export default profile
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    padding: 16,
+    gap: 16,
+  },
+  imageContainer: {
+    width: 100,
+    height: 100,
+    borderRadius: 50,
+  },
+  infoContainer: {
+    alignItems: 'center',
+  },
+  userName: {
+    fontSize: 24,
+    fontWeight: 'bold',
+  },
+})
