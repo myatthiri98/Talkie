5f0fa14c66adc1126e049a79d5e3a63645c013d7
Myatthiri <myatthirikhin1998@gmail.com>
2025-07-26 19:30:50 +0630

    feat: add sign-out functionality to profile component with button integration

--- app/(chat)/profile.tsx
+++ app/(chat)/profile.tsx
@@ -1,11 +1,24 @@
+import { Button } from '@/components/Button'
 import CustomText from '@/components/Text'
+import { useAuth, useUser } from '@clerk/clerk-expo'
+import { useRouter } from 'expo-router'
 import React from 'react'
 import { View } from 'react-native'
 
 const profile = () => {
+  const { user } = useUser()
+  const { signOut } = useAuth()
+  const router = useRouter()
+
+  const handleSignOut = async () => {
+    await signOut()
+    router.replace('/(auth)')
+  }
+
   return (
     <View>
       <CustomText>profile</CustomText>
+      <Button onPress={handleSignOut}> Sign Out</Button>
     </View>
   )
 }
